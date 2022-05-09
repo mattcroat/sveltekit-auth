ae6b45714cf5c6ebb5d43e02659053ba119e3e6e
mattcroat <mattcroat.dev@gmail.com>
2022-05-09 21:46:36 +0200

    feat: Redirect if user is logged in

--- src/routes/register/index.svelte
+++ src/routes/register/index.svelte
@@ -1,3 +1,18 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = ({ session }) => {
+    if (session.user) {
+      return {
+        status: 302,
+        redirect: '/',
+      }
+    }
+
+    return {}
+  }
+</script>
+
 <script lang="ts">
   import { goto } from '$app/navigation'
 
